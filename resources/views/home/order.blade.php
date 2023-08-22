620ab7990db9f3509b1b0998202bb4f593d84fad
AkmalYonan <akmalyonanda@gmail.com>
2023-08-22 08:38:34 +0700

    Fixing Layout Order Page

--- resources/views/home/order.blade.php
+++ resources/views/home/order.blade.php
@@ -34,14 +34,12 @@
       </div>
       <div class="col-md-8">
         <div class="card shadow-lg mt-5">
-          <div class="card-header">
-            <p class="fs-5 fw-bold text-center">Form Order Pesanan</p>
-          </div>
+          <p class="fs-3 pt-4 fw-bold text-center text-lead2">Form Order Pesanan</p>
           <div class="card-body">
             <form action="/order-pesan" method="POST" enctype="multipart/form-data">
               @csrf
-              <div class="row mx-2 justify-content-center text-center">
-                <div class="col-md-8 px-4">
+              <div class="row justify-content-center text-center">
+                <div class="col-md-11">
                   <div class="form-group py-2 mb-3 pt-3">
                     @if(isset($templates))
                     <label for="template_id">Pilih desain:</label>
@@ -74,98 +72,111 @@
                   </div>
                 </div>
               </div>
-              <div class="row justify-content-center pt-5 pb-2 gap-5">
-                <div class="col-md-10 card py-3 px-3 shadow-lg">
-                  <div class="form-group mb-3">
-                    <label for="nama_mempelai_pria">Nama Mempelai Pria:</label>
+              <div class="row justify-content-center pt-5 gap-5">
+                <div class="col-md-11">
+                  <p>Data Pria</p>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_mempelai_pria" name="nama_mempelai_pria"
                       value="{{ old('nama_mempelai_pria') }}" required>
+                    <label for="nama_mempelai_pria">Nama Mempelai Pria:</label>
+
                   </div>
-                  <div class=" form-group mb-3">
-                    <label for="anak_ke_pria">Anak Ke:</label>
+                  <div class=" form-floating mb-3">
                     <input type="number" class="form-control" id="anak_ke_pria" name="anak_ke_pria"
                       value="{{ old('anak_ke_pria') }}" required>
+                    <label for="anak_ke_pria">Anak Ke:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="nama_ayah_pria">Nama Ayah:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_ayah_pria" name="nama_ayah_pria"
                       value="{{ old('nama_ayah_pria') }}" required>
+                    <label for="nama_ayah_pria">Nama Ayah:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="nama_ibu_pria">Nama Ibu:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_ibu_pria" name="nama_ibu_pria"
                       value="{{ old('nama_ibu_pria') }}" required>
+                    <label for="nama_ibu_pria">Nama Ibu:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="username_ig_pria">Username Instagram:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="username_ig_pria" name="username_ig_pria"
                       value="{{ old('username_ig_pria') }}" required>
+                    <label for="username_ig_pria">Username Instagram:</label>
+
                   </div>
                 </div>
 
-                <div class="col-md-10 card py-3 px-3 shadow-lg">
-                  <div class="form-group mb-3">
-                    <label for="nama_mempelai_wanita">Nama Mempelai Wanita:</label>
+                <div class="col-md-11">
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_mempelai_wanita" name="nama_mempelai_wanita"
                       value="{{ old('nama_mempelai_wanita') }}" required>
+                    <label for="nama_mempelai_wanita">Nama Mempelai Wanita:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="anak_ke_wanita">Anak Ke:</label>
+                  <div class="form-floating mb-3">
                     <input type="number" class="form-control" id="anak_ke_wanita" name="anak_ke_wanita"
                       value="{{ old('anak_ke_wanita') }}" required>
+                    <label for="anak_ke_wanita">Anak Ke:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="nama_ayah_wanita">Nama Ayah:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_ayah_wanita" name="nama_ayah_wanita"
                       value="{{ old('nama_ayah_wanita') }}" required>
+                    <label for="nama_ayah_wanita">Nama Ayah:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="nama_ibu_wanita">Nama Ibu:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_ibu_wanita" name="nama_ibu_wanita"
                       value="{{ old('nama_ibu_wanita') }}" required>
+                    <label for="nama_ibu_wanita">Nama Ibu:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="username_ig_wanita">Username Instagram:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="username_ig_wanita" name="username_ig_wanita"
                       value="{{ old('username_ig_pria') }}" required>
+                    <label for="username_ig_wanita">Username Instagram:</label>
+
                   </div>
                 </div>
               </div>
-              <div class="row justify-content-center py-5 gap-5">
-                <div class="col-md-10 card py-3 px-3 shadow-lg">
-                  <div class="form-group mb-3">
-                    <label for="lokasi_acara">Lokasi Acara:</label>
+              <div class="row justify-content-center gap-5">
+                <div class="col-md-11">
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="lokasi_acara" name="lokasi_acara" required>
+                    <label for="lokasi_acara">Lokasi Acara:</label>
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="tgl_akad">Tanggal Akad:</label>
+                  <div class="form-floating mb-3">
                     <input type="date" class="form-control" id="tgl_akad" name="tgl_akad" required>
+                    <label for="tgl_akad">Tanggal Akad:</label>
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="tgl_resepsi">Tanggal Resepsi:</label>
+                  <div class="form-floating mb-3">
                     <input type="date" class="form-control" id="tgl_resepsi" name="tgl_resepsi">
+                    <label for="tgl_resepsi">Tanggal Resepsi:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="jam_acara">Jam Acara:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="jam_acara" name="jam_acara" required>
+                    <label for="jam_acara">Jam Acara:</label>
                   </div>
                 </div>
-                <div class="col-md-10 card py-3 px-3 shadow-lg">
-                  <div class="form-group mb-3">
-                    <label for="email">Email:</label>
+                <div class="col-md-11">
+                  <div class="form-floating mb-3">
                     <input type="email" class="form-control" id="email" name="email" required>
+                    <label for="email">Email:</label>
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="no_wa">Nomor WhatsApp:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="no_wa" name="no_wa" required>
+                    <label for="no_wa">Nomor WhatsApp:</label>
+
                   </div>
-                  <div class="form-group mb-3">
-                    <label for="nama_panggilan">Nama Panggilan:</label>
+                  <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nama_panggilan" name="nama_panggilan" required>
+                    <label for="nama_panggilan">Nama Panggilan:</label>
                   </div>
                   @if(isset($fiturs))
-                  <div class="form-group py-2">
-                    <label>Select fiturs:</label>
+                  <label>Select fiturs:</label>
+                  <div class="form-floating py-2">
                     @foreach($fiturs as $fitur)
                     @if($loop->iteration < 7) <div class="form-check">
                       <input type="checkbox" name="selected_fiturs[]" value="{{ $fitur->id }}" class="form-check-input"
@@ -183,36 +194,36 @@
                   @endif
                 </div>
               </div>
-              <div class="row justify-content-center py-5 gap-5">
-                <div class="col-md-10 card py-3 px-3 shadow-lg">
-                  <div class=" mb-3">
-                    <label for="fotoPria" class="form-label">Foto Mempelai Pria</label>
+              <div class="row justify-content-center gap-5">
+                <div class="col-md-11">
+                  <div class="mb-3">
+                    <label for="fotoPria">Foto Mempelai Pria</label>
                     <input class="form-control" type="file" id="fotoPria" name="fotoPria">
                   </div>
                   <div class=" mb-3">
-                    <label for="fotoWanita" class="form-label">Foto Mempelai Wanita</label>
+                    <label for="fotoWanita">Foto Mempelai Wanita</label>
                     <input class="form-control" type="file" id="fotoWanita" name="fotoWanita">
                   </div>
                   <div class=" mb-3">
-                    <label for="fotoCouple" class="form-label">Foto Couple ( 1 Frame )</label>
+                    <label for="fotoCouple">Foto Couple ( 1 Frame )</label>
                     <input class="form-control" type="file" id="fotoCouple" name="fotoCouple">
                   </div>
                   <div class=" mb-3">
-                    <label for="fotoThumbnail" class="form-label">Foto Thumbnail Undangan</label>
+                    <label for="fotoThumbnail">Foto Thumbnail Undangan</label>
                     <input class="form-control" type="file" id="fotoThumbnail" name="fotoThumbnail">
                   </div>
                   <div class=" mb-3">
-                    <label for="fotoBanner" class="form-label">Foto Banner</label>
+                    <label for="fotoBanner">Foto Banner</label>
                     <input class="form-control" type="file" id="fotoBanner" name="fotoBanner">
                   </div>
-                  <div class=" mb-3">
-                    <label for="fotoGallery" class="form-label">Foto Gallery ( Max 6 )</label>
+                  <div class="mb-3">
+                    <label for="fotoGallery">Foto Gallery ( Max 6 )</label>
                     <input class="form-control" type="file" id="fotoGallery" name="fotoGallery[]" multiple>
                   </div>
                 </div>
               </div>
               <div class="container text-center">
-                <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                <button type="submit" class="btn btn-primary btn-block w-75">Submit</button>
               </div>
             </form>
           </div>
