e936963f2c92e7231e4a69cca831d9236925deee
Akmal Bariq <akmalyonanda@gmail.com>
2023-08-22 14:50:30 +0700

    Change ConfirmMail to Gmail

--- resources/views/emails/notifconfirm.blade.php
+++ resources/views/emails/notifconfirm.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Email Confirmation</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
 
@@ -26,8 +29,9 @@
         .container {
             max-width: 800px;
             padding: 20px;
-            background-image: url("https://github.com/bakaroti/resource/blob/main/liq2.jpg?raw=true");
-            background-size: cover;
+            background-color: #fff
+                /* background-image: url("https://github.com/bakaroti/resource/blob/main/liq3.png?raw=true"); */
+                background-size: cover;
             background-repeat: no-repeat;
             background-position: center;
             border: 1px solid #ddd;
@@ -66,29 +70,39 @@
 
 <body>
     <div class="container">
-        <table class="table table-bordered text-center rounded rounded-3 overflow-hidden">
-            <thead>
-                <tr class="table-dark">
-                    <th scope="col">ID Pesan</th>
-                    <th scope="col">ID User</th>
-                    <th scope="col">Nama User</th>
-                    <th scope="col">Email User</th>
-                    <th scope="col">Nomor User</th>
-                    <th scope="col">Link Gmap</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr>
-                    <td>{{ $pesan->id }}</td>
-                    <td>{{ $pesan->id_user }}</td>
-                    <td>{{ $pesan->user->name }}</td>
-                    <td>{{ $pesan->data->email }}</td>
-                    <td class="btn btn-link" onclick="location.href='https://wa.me/{{ $pesan->data->no_wa }}'">
-                        https://wa.me/{{ $pesan->data->no_wa }}</td>
-                    <td>{{ $pesan->data->iframeMaps }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="container">
+            <div class="logo-container">
+                <img class="logo" src="https://github.com/bakaroti/resource/blob/main/logo_110.png?raw=true" alt="logo"
+                    width="100">
+            </div>
+            <div class="table-responsive">
+                <table class="table table-bordered text-center rounded rounded-3 overflow-hidden">
+                    <thead>
+                        <tr class="table-dark">
+                            <th scope="col">ID Pesan</th>
+                            <th scope="col">ID User</th>
+                            <th scope="col">Nama User</th>
+                            <th scope="col">Email User</th>
+                            <th scope="col">Nomor User</th>
+                            <th scope="col">Link Gmap</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td>{{ $pesan->id }}</td>
+                            <td>{{ $pesan->id_user }}</td>
+                            <td>{{ $pesan->user->name }}</td>
+                            <td>{{ $pesan->data->email }}</td>
+                            <td onclick="location.href='https://wa.me/{{ $pesan->data->no_wa }}'">
+                                <a class="btn btn-primary">click me</a>
+                            </td>
+                            <td>{{ $pesan->data->iframeMaps }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </div>
 </body>
 
