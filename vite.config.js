a9653f078b57721304ff173290a87c7fef407a5b
AkmalYonan <akmalyonanda@gmail.com>
2026-02-26 22:54:26 +0700

    Upgrade laravel-vite-plugin to 2.1.0 and silence SCSS deprecations

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,23 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/tailwind.css",
-                "resources/sass/app.scss",
-                "resources/js/app.js",
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Ini akan membisukan peringatan deprecation
+                quietDeps: true,
+                // Jika masih muncul, tambahkan ini (untuk versi Sass terbaru)
+                silenceDeprecations: [
+                    "import",
+                    "global-builtin",
+                    "color-functions",
+                    "if-function",
+                ],
+            },
+        },
+    },
 });
